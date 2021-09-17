660ed391566a5acc13117be20b8019ef4f0b00cb
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-09-17 15:04:55 +0100

    Clean up jojo file

--- commands/gifs/jojo.js
+++ commands/gifs/jojo.js
@@ -1,52 +1,35 @@
-//@ts-check 
-
-const {CommandInteraction} = require('discord.js');
+// @ts-check
 const {SlashCommandBuilder} = require('@discordjs/builders');
 const fs = require('fs');
 
+const name = 'jojo';
+const description = 'Replies with a random jojo gif!';
+
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('jojo')
-        .setDescription('Replies with a random jojo gif!'),
+    data: new SlashCommandBuilder().setName(name).setDescription(description),
     /**
-     * @param {CommandInteraction} interaction
+     * @param {import('discord.js').CommandInteraction} interaction
      * @returns {Promise<void>}
      */
     execute(interaction) {
         try {
-            const linkArray = fs
-                .readFileSync('././resources/gifs/jojolinks.txt', 'utf8')
-                .split('\n');
+            const linkArray = fs.readFileSync('././resources/gifs/jojolinks.txt', 'utf8').split('\n');
             const link = linkArray[Math.floor(Math.random() * linkArray.length)];
-            interaction.reply(link);
-            return;
-
-            /*
-      I changed the command from calling the tenor api each time someone
-      uses the !jojo command for 2 main reasons:
-
-      1. The tenor api doesn't always respond with a valid jojo gif, sometimes
-      it responds with a wrong gif.
-      2. Instead of waiting for the api we can just pick a random link from
-      the jojolinks file so the response is faster.
-      You can still use the old method, it's commented out down below, and
-      don't forget to uncomment the require for node-fetch and tenorAPI above
-      */
-
-            /*
-      fetch(
-        `https://g.tenor.com/v1/random?key=${tenorAPI}&q=jojos-bizarre-adventure&limit=1`
-      )
-        .then(res => res.json())
-        .then(json => message.channel.send(json.results[0].url))
-        .catch(e => {
-          message.reply('Failed to fetch a gif :slight_frown:');
-          return console.error(e);
-        })
-      */
+            return interaction.reply(link);
         } catch(e) {
-            interaction.reply(':x: Failed to fetch a gif!');
-            return console.error(e);
+            console.error(e);
+            return interaction.reply(':x: Failed to fetch a gif!');
         }
     }
 };
+
+
+/*
+    fetch(`https://g.tenor.com/v1/random?key=${tenorAPI}&q=jojos-bizarre-adventure&limit=1`)
+        .then((res) => res.json())
+        .then((json) => message.channel.send(json.results[0].url))
+        .catch(e => {
+            console.error(e);
+            return message.reply('Failed to fetch a gif :slight_frown:');
+        })
+*/
