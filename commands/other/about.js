92ca47f627e9abd434000423ded2425c2dc96a90
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-09-17 12:08:52 +0100

    Fix typo in about

--- commands/other/about.js
+++ commands/other/about.js
@@ -4,6 +4,6 @@ const {SlashCommandBuilder} = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder().setName('about').setDescription('Info about the bot and its creator!'),
     execute(interaction) {
-        return interaction.reply('Made by @DanielRX#669 with :heart: code is available on GitHub (coming soon)');
+        return interaction.reply('Made by @DanielRX#6669 with :heart: code is available on GitHub (coming soon)');
     }
 };
