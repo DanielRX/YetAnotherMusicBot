3e046ce98c1aeea4a7a36e55aec33e3a4943a08d
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-09-18 22:20:25 +0100

    Clean up animegif.js (final form)

--- commands/gifs/animegif.js
+++ commands/gifs/animegif.js
@@ -3,21 +3,23 @@ const {SlashCommandBuilder} = require('@discordjs/builders');
 const fetch = require('node-fetch');
 const {tenorAPI} = require('../../config.json');
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('animegif')
-        .setDescription('Responds with a random anime gif'),
-    /**
-     * @param {import('discord.js').CommandInteraction} interaction
-     * @returns {Promise<void>}
-     */
-    async execute(interaction) {
-        if(!tenorAPI) { return interaction.reply(':x: Tenor commands are not enabled'); }
-        fetch(`https://g.tenor.com/v1/random?key=${tenorAPI}&q=anime&limit=50`)
-            .then((res) => res.json())
-            .then((json) => interaction.reply(json.results[Math.floor(Math.random() * 49)].url))
-            .catch(function onError() {
-                return interaction.reply(':x: Failed to find a gif!');
-            });
-    }
+const name = 'animegif';
+const description = 'Responds with a random anime gif';
+
+const data = new SlashCommandBuilder().setName(name).setDescription(description);
+
+/**
+ * @param {import('discord.js').CommandInteraction} interaction
+ * @returns {Promise<void>}
+ */
+const execute = async(interaction) => {
+    if(!tenorAPI) { return interaction.reply(':x: Tenor commands are not enabled'); }
+    fetch(`https://g.tenor.com/v1/random?key=${tenorAPI}&q=anime&limit=50`)
+        .then((res) => res.json())
+        .then((json) => interaction.reply(json.results[Math.floor(Math.random() * 49)].url))
+        .catch(() => {
+            return interaction.reply(':x: Failed to find a gif!');
+        });
 };
+
+module.exports = {data, execute};
