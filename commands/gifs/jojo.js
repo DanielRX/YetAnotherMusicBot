b8daca8cfadcf4a27a6346c4e69438cb71b236c5
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-09-18 22:30:49 +0100

    Clean up jojo.js

--- commands/gifs/jojo.js
+++ commands/gifs/jojo.js
@@ -6,23 +6,24 @@ const {randomEl} = require('../../utils/utils');
 const name = 'jojo';
 const description = 'Replies with a random jojo gif!';
 
-module.exports = {
-    data: new SlashCommandBuilder().setName(name).setDescription(description),
-    /**
-     * @param {import('discord.js').CommandInteraction} interaction
-     * @returns {Promise<void>}
-     */
-    async execute(interaction) {
-        try {
-            const linkArray = await fs.readFile('./resources/gifs/jojolinks.txt', 'utf8').then((links) => links.split('\n'));
-            const link = randomEl(linkArray);
-            return interaction.reply(link);
-        } catch(e) {
-            console.error(e);
-            return interaction.reply(':x: Failed to fetch a gif!');
-        }
+const data = new SlashCommandBuilder().setName(name).setDescription(description);
+
+/**
+* @param {import('discord.js').CommandInteraction} interaction
+* @returns {Promise<void>}
+*/
+const execute = async(interaction) => {
+    try {
+        const linkArray = await fs.readFile('./resources/gifs/jojolinks.txt', 'utf8').then((links) => links.split('\n'));
+        const link = randomEl(linkArray);
+        return interaction.reply(link);
+    } catch(e) {
+        console.error(e);
+        return interaction.reply(':x: Failed to fetch a gif!');
     }
 };
+
+module.exports = {data, execute};
 
 /*
     fetch(`https://g.tenor.com/v1/random?key=${tenorAPI}&q=jojos-bizarre-adventure&limit=1`)
