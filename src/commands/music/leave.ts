1ae3846f526c7129d8213a4e9ae73f4a09612bad
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-10-03 22:51:42 +0100

    Fix leave strings

--- src/commands/music/leave.ts
+++ src/commands/music/leave.ts
@@ -1,21 +1,23 @@
 import {AudioPlayerStatus} from '@discordjs/voice';
 import type {CustomInteraction} from '../../utils/types';
 import {playerManager} from '../../utils/client';
+import {getAndFillMessage} from '../../utils/messages';
 
 export const name = 'leave';
 export const description = 'Leaves a voice channel if in one!';
 export const deferred = false;
 
 export const execute = async(interaction: CustomInteraction): Promise<string> => {
+    const message = getAndFillMessage('leave', 'en_gb'); // TODO: User/server locale?
     const voiceChannel = interaction.member.voice.channel;
-    if(!voiceChannel) { return 'Please join a voice channel and try again!'; }
+    if(!voiceChannel) { return message('NOT_IN_VC'); }
 
     const player = playerManager.get(interaction.guildId);
-    if(typeof player === 'undefined' || player.audioPlayer.state.status !== AudioPlayerStatus.Playing) { return 'There is no song playing right now!'; }
-    if(voiceChannel.id !== interaction.guild.me?.voice.channel?.id) { return 'You must be in the same voice channel as the bot in order to skip!'; }
+    if(typeof player === 'undefined' || player.audioPlayer.state.status !== AudioPlayerStatus.Playing) { return message('NO_SONG_PLAYING'); }
+    if(voiceChannel.id !== interaction.guild.me?.voice.channel?.id) { return message('NOT_IN_SAME_VC'); }
 
     player.connection.destroy();
     playerManager.delete(interaction.guildId);
-    return 'Left your voice channel!';
+    return message('LEFT_VC');
 };
 
