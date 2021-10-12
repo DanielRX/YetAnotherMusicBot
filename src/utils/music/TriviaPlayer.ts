92c69f27186d0ee667c3f607b2533f87beeedfc0
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-10-12 18:36:52 +0100

    Fix?

--- src/utils/music/TriviaPlayer.ts
+++ src/utils/music/TriviaPlayer.ts
@@ -39,7 +39,7 @@ const getLeaderBoard = (arr: [string, number][]) => { // TODO: Shared medals
     const cleanPlayer = (player: string) => {
         const [platform, username] = player.split(':');
         if(platform === 't') {
-            return `<:twitch:897537645398351942> ${username}`;
+            return `<:twitch:897531638144184370> ${username}`;
         }
         return `<:discord:897536633145032706> ${username}`;
     };
