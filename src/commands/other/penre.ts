1602f90b3a9c4f6b81bcff6eadd39c77ee6421f6
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-10-16 00:25:57 +0100

    Add penre command

--- src/commands/other/penre.ts
+++ src/commands/other/penre.ts
@@ -2,12 +2,12 @@ import type {CommandReturn} from '../../utils/types';
 import fs from 'fs-extra';
 import {randomEl} from '../../utils/utils';
 
-export const name = 'elina';
-export const description = 'Replies with an elina gif/video!';
+export const name = 'penre';
+export const description = 'Replies with an penre gif/video!';
 export const deferred = false;
 
 export const execute = async(): Promise<CommandReturn> => {
-    const linkArray = await fs.readFile('./resources/media/elina.txt', 'utf8').then((links) => links.split('\n'));
+    const linkArray = await fs.readFile('./resources/media/penre.txt', 'utf8').then((links) => links.split('\n'));
     const link = randomEl(linkArray);
     return link;
 };
