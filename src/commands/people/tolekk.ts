27a7201d953e0e1e1a1e84a115ab1992049b5696
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-10-22 10:45:46 +0100

    Add tolekk command

---
+++ src/commands/people/tolekk.ts
@@ -0,0 +1,13 @@
+import type {CommandReturn} from '../../utils/types';
+import fs from 'fs-extra';
+import {randomEl} from '../../utils/utils';
+
+export const name = 'tolekk';
+export const description = 'Replies with a tolekk gif/video!';
+export const deferred = false;
+
+export const execute = async(): Promise<CommandReturn> => {
+    const linkArray = await fs.readFile('./resources/media/tolekk.txt', 'utf8').then((links) => links.split('\n'));
+    const link = randomEl(linkArray);
+    return link;
+};
