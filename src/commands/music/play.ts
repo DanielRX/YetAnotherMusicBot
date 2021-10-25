aa14302db48824e8a5508d96e99bc1859fe5e58e
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-10-25 23:27:22 +0100

    Type fix

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -550,7 +550,7 @@ export const execute = async({interaction, guildId, messages, params: {rawQuery,
         const x = await searchYoutube(interaction, rawQuery, flags, interaction.member.voice.channel as VoiceChannel);
         player.commandLock = false;
         return x;
-    } catch(e: Error) {
+    } catch(e: unknown) {
         logger.error(e);
         player.commandLock = false;
     }
