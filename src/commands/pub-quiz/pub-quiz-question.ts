b0f8aa944dd28d69d8a6a083a06dca6705695111
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-10-31 16:54:38 +0000

    Add pub-quiz-question

---
+++ src/commands/pub-quiz/pub-quiz-question.ts
@@ -0,0 +1,8 @@
+import type {CommandReturn, CommandInput} from '../../utils/types';
+
+export const name = 'pub-quiz-question';
+export const description = 'Replies with a pub quiz question!';
+export const deferred = false;
+
+export const execute = async({messages}: CommandInput): Promise<CommandReturn> => 'Work in progress!';
+
