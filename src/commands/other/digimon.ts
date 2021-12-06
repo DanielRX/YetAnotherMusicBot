4d8b2e8bbc5249f61a6fadce840ccb8bd8f1dfbd
DanielRX <16984675+DanielRX@users.noreply.github.com>
2021-12-06 21:41:04 +0000

    Add digimon command

---
+++ src/commands/other/digimon.ts
@@ -0,0 +1,30 @@
+import type {CommandInput, CommandReturn} from '../../utils/types';
+import {fetchJSON} from '../../utils/utils';
+import {MessageEmbed} from 'discord.js';
+
+export const name = 'digmon';
+export const description = 'Get the info on a digimon!';
+export const deferred = false;
+
+type DigmonInfo = {
+  name: string,
+  img: string,
+  level: string,
+};
+
+export const options = [
+    {type: 'string' as const, name: 'name', description: 'Which digimon do you want to see?', required: true, choices: []}
+];
+
+const url = `https://digimon-api.vercel.app/api/digimon/name/`;
+
+export const execute = async({messages, params: {name: digimon}}: CommandInput<{name: string}>): Promise<CommandReturn> => {
+    const json = await fetchJSON<DigmonInfo>(`${url}/${digimon}`);
+    const embed = new MessageEmbed()
+        .setDescription(json.name)
+        .setTimestamp()
+        .setTitle(json.name)
+        .setURL(json.img)
+        .setFooter(`${messages.POWERED_BY()} digimon-api.vercel.app`);
+    return {embeds: [embed]};
+};
